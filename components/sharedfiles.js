7b0ffe7311b772fe915584ff50a2d9a8def77635
3urobeat <35304405+3urobeat@users.noreply.github.com>
2024-07-27 12:57:15 +0200

    feat: Include vote support in my fork

--- components/sharedfiles.js
+++ components/sharedfiles.js
@@ -250,3 +250,69 @@ SteamCommunity.prototype.unsubscribeWorkshopSharedFile = function(sharedFileId, 
 		callback(err);
 	}, "steamcommunity");
 };
+
+/**
+ * Downvotes a sharedfile
+ * @param {string} sid - ID of the sharedfile
+ * @param {function} callback - Takes only an Error object/null as the first argument
+ */
+SteamCommunity.prototype.voteDownSharedFile = function(sid, callback) {
+	this.httpRequestPost({
+		"uri": "https://steamcommunity.com/sharedfiles/votedown",
+		"form": {
+			"id": sid,
+			"json": "1",
+			"sessionid": this.getSessionID()
+		},
+		"json": true
+	}, function(err, response, body) {
+		if (!callback) {
+			return;
+		}
+
+		if (err) {
+			callback(err);
+			return;
+		}
+
+		if (body.success && body.success != SteamCommunity.EResult.OK) {
+			callback(Helpers.eresultError(body.success));
+			return;
+		}
+
+		callback(null);
+	}, "steamcommunity");
+};
+
+/**
+ * Upvotes a sharedfile
+ * @param {string} sid - ID of the sharedfile
+ * @param {function} callback - Takes only an Error object/null as the first argument
+ */
+SteamCommunity.prototype.voteUpSharedFile = function(sid, callback) {
+	this.httpRequestPost({
+		"uri": "https://steamcommunity.com/sharedfiles/voteup",
+		"form": {
+			"id": sid,
+			"json": "1",
+			"sessionid": this.getSessionID()
+		},
+		"json": true
+	}, function(err, response, body) {
+		if (!callback) {
+			return;
+		}
+
+		if (err) {
+			callback(err);
+			return;
+		}
+
+		if (body.success && body.success != SteamCommunity.EResult.OK) {
+			callback(Helpers.eresultError(body.success));
+			return;
+		}
+
+		callback(null);
+	}, "steamcommunity");
+};
